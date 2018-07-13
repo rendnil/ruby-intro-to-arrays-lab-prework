53910b4c6dcf6aea29e3af2762dc6f0f27b7732d
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-13 17:34:46 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,10 +14,32 @@ def first_element(array)
 
 end  
 
-
+def third_element(array)
+  
+  array[2]
+  
+end
 
 def last_element(array)
   
   array[-1]
+  
+end
+
+def first_element_with_array_methods(array)
+  
+  array.first
+  
+end
+
+def last_element_with_array_methods(array)
+  
+  array.last
+  
+end
+
+def length_of_array(array)
+  
+  array.length
   
 end
