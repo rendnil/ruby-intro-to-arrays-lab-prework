43b9cbf71465261a885ecc6f22070c0881b8f899
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-13 17:31:03 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+  [ ]
+  
+end  
+
+def array_with_two_elements
+  ["a","b"]
+  
+end
+
+def first_element(array)
+  
+  array[0]
+
+end  
+
+
+
+def last_element(array)
+  
+  array[-1]
+  
+end
